39a478d55985f9104f2418e43d7a55c0d20b7a61
MarioKarv <mariook0204@gmail.com>
2023-03-09 11:47:15 +0200

    2 задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,14 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const time = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(updatePlaybackTime, 1000));
+
+function updatePlaybackTime({ seconds }) {
+    localStorage.setItem(time, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(time));
